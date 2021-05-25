6337781e03b3a60097eedfcb839534301fc8bed9
Laura27-89 <79119989+Laura27-89@users.noreply.github.com>
2021-05-24 20:53:29 -0600

    hobby y edad del papa

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Laura alumna = new Laura();
+ //       Laura alumna = new Laura();
         Jorge papa = new Jorge("Hola Hija");
+        papa.setAge();
+        int age = papa.getAge();
+        String hobby = papa.getHobby();
+        System.out.println("Yo tengo: "+ papa.getAge() + "." + " Y me gusta " + papa.getHobby());
     }
 }
--- src/com/ucreativa/familia/Jorge.java
+++ src/com/ucreativa/familia/Jorge.java
@@ -1,8 +1,25 @@
 package com.ucreativa.familia;
 
 public class Jorge {
+
+    private int age;
+    private String hobby;
+
         public Jorge(String saludo) {
+            this.age = 60;
+            this.hobby = "Jugar Futball";
             System.out.println(saludo);
         }
 
+    public void setAge() {
+            this.age = this.age +1;
+    }
+
+    public int getAge(){
+            return this.age;
+        }
+
+    public String getHobby() {
+        return this.hobby;
+    }
 }
