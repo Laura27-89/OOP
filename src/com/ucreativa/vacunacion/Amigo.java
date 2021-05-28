61f989ec0099b32295069ecab5e24c890e506669
Laura27-89 <79119989+Laura27-89@users.noreply.github.com>
2021-05-27 21:07:50 -0600

    Herencias: vacunas familia amigo persona

--- src/com/ucreativa/vacunacion/Amigo.java
+++ src/com/ucreativa/vacunacion/Amigo.java
@@ -5,7 +5,7 @@ public class Amigo extends Persona {
     private String relacion;
     private String facebook;
 
-    public Amigo(String nombre, String cedula, int edad, Boolean riesgo, String relacion, String facebook) {
+    public Amigo(String nombre, String cedula, int edad, boolean riesgo, String relacion, String facebook) {
         super(nombre, cedula, edad, riesgo);
         this.relacion = relacion;
         this.facebook = facebook;
--- src/com/ucreativa/vacunacion/Familiar.java
+++ src/com/ucreativa/vacunacion/Familiar.java
@@ -4,7 +4,7 @@ public class Familiar extends Persona {
 
     private String parestesco;
 
-    public Familiar(String nombre, String cedula, int edad, Boolean riesgo, String parestesco) {
+    public Familiar(String nombre, String cedula, int edad, boolean riesgo, String parestesco) {
         super(nombre, cedula, edad, riesgo);
         this.parestesco = parestesco;
     }
