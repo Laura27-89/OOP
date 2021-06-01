6c50f17089efb7c22ee228e8cf0a45aa9dc25fe1
Laura27-89 <79119989+Laura27-89@users.noreply.github.com>
2021-05-31 20:14:35 -0600

    union con la clase de vacuna, clase 6

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -2,6 +2,12 @@ package com.ucreativa;
 
 import com.ucreativa.familia.Laura;
 import com.ucreativa.familia.Jorge;
+import com.ucreativa.vacunacion.Amigo;
+import com.ucreativa.vacunacion.Familiar;
+import com.ucreativa.vacunacion.Persona;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,6 +19,22 @@ public class Main {
         int age = papa.getAge();
         String hobby = papa.getHobby();
         System.out.println("Yo tengo: "+ papa.getAge() + "." + " Y me gusta " + papa.getHobby());
+
+        Familiar hermano = new Familiar("Andres", "5678", 33,
+                true, "Hermano");
+
+        Familiar mama = new Familiar("Marianella", "7654", 59,
+                false, "Mama");
+
+        Amigo trabajo = new Amigo("Irene", "8765", 32,
+                true, "Amiga de trabajo", "Irene45");
+
+
+        List<Persona> familia = new ArrayList<>();
+        familia.add(mama);
+        familia.add(hermano);
+        familia.add(trabajo);
+
     }
 }
 
