1e86446432def149ec2106f2b87178065dfd7174
Laura27-89 <79119989+Laura27-89@users.noreply.github.com>
2021-06-03 21:00:42 -0600

    campos de consola y copia del main anterior, clase8

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -8,29 +8,51 @@ import com.ucreativa.vacunacion.entities.Persona;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<BitacoraVacunas> db = new ArrayList<>();
+        while (true) {
+            String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca;
+            Persona persona;
+            System.out.println("Nombre:");
+            nombre = in.nextLine();
+            System.out.println("Cedula:");
+            cedula = in.nextLine();
+            System.out.println("Edad:");
+            edad = in.nextLine();
+            System.out.println("Riesgo(S/N):");
+            riesgo = in.nextLine();
+            System.out.println("Amigo(A/F):");
+            isAmigo = in.nextLine();
+            if (isAmigo.equals("A")) {
+                System.out.println("Relacion:");
+                relacion = in.nextLine();
+                System.out.println("Facebook:");
+                facebook = in.nextLine();
+                persona = new Amigo(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), relacion, facebook);
+            } else {
+                System.out.println("Parentesco:");
+                parentesco = in.nextLine();
+                persona = new Familiar(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), parentesco);
+            }
+            System.out.println("Vacuna --Marca:");
+            marca = in.nextLine();
+            db.add(new BitacoraVacunas(persona, marca, new Date()));
 
-        Familiar hermano = new Familiar("Andres", "5678", 33,
-                true, "Hermano");
+            System.out.println("Quiere imprimir Lista (S)");
+            String print = in.nextLine();
+            if (print.equals("S")) {
+                for (BitacoraVacunas item : db) {
+                    System.out.println(item.getPersona().getNombre() + " vacunado el " + item.getFecha());
+                }
+            }
 
-        Familiar mama = new Familiar("Marianella", "7654", 59,
-                false, "Mama");
+        }//while
+    }//main
+}//close
 
-        Amigo trabajo = new Amigo("Irene", "8765", 32,
-                true, "Amiga de trabajo", "Irene45");
-
-
-        List<Persona> familia = new ArrayList<>();
-        familia.add(mama);
-        familia.add(hermano);
-        familia.add(trabajo);
-
-        List<BitacoraVacunas> bitacora = new ArrayList<>();
-        bitacora.add(new BitacoraVacunas(mama, "Pfizer", new Date()));
-        bitacora.add(new BitacoraVacunas(hermano, "Astrazeneca", new Date()));
-        bitacora.add(new BitacoraVacunas(trabajo, "Johnson", new Date()));
-    }
-}
+///Integer.parseInt, sirve para pasar de string a numero
---
+++ src/com/ucreativa/vacunacion/Main1old.java
@@ -0,0 +1,37 @@
+package com.ucreativa.vacunacion;
+
+
+import com.ucreativa.vacunacion.entities.Amigo;
+import com.ucreativa.vacunacion.entities.BitacoraVacunas;
+import com.ucreativa.vacunacion.entities.Familiar;
+import com.ucreativa.vacunacion.entities.Persona;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main1old {
+
+    public static void main(String[] args) {
+        Familiar hermano = new Familiar("Andres", "5678", 33,
+                true, "Hermano");
+
+        Familiar mama = new Familiar("Marianella", "7654", 59,
+                false, "Mama");
+
+        Amigo trabajo = new Amigo("Irene", "8765", 32,
+                true, "Amiga de trabajo", "Irene45");
+
+
+        List<Persona> familia = new ArrayList<>();
+        familia.add(mama);
+        familia.add(hermano);
+        familia.add(trabajo);
+
+        List<BitacoraVacunas> bitacora = new ArrayList<>();
+        bitacora.add(new BitacoraVacunas(mama, "Pfizer", new Date()));
+        bitacora.add(new BitacoraVacunas(hermano, "Astrazeneca", new Date()));
+        bitacora.add(new BitacoraVacunas(trabajo, "Johnson", new Date()));
+    }
+}
