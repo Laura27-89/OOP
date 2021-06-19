2fc6ef0d5d44e65860127904554ad7ce45ee74c4
Laura27-89 <79119989+Laura27-89@users.noreply.github.com>
2021-06-19 14:05:47 -0600

    Exception clase 12

--- src/com/ucreativa/vacunacion/services/BitacoraService.java
+++ src/com/ucreativa/vacunacion/services/BitacoraService.java
@@ -4,6 +4,7 @@ import com.ucreativa.vacunacion.entities.Amigo;
 import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
 import com.ucreativa.vacunacion.repositories.Repository;
+import com.ucreativa.vacunacion.ui.ErrorEnEdadException;
 
 import java.util.Date;
 import java.util.List;
@@ -19,9 +20,14 @@ public class BitacoraService {
 
     public void save(String nombre, String cedula, String txtEdad,
                      boolean riesgo, boolean isAmigo,String relacion,
-                     String facebook, String parentesco, String marca){
+                     String facebook, String parentesco, String marca) throws ErrorEnEdadException {
 
-        int edad = Integer.parseInt(txtEdad);
+        int edad = 0;
+        try {
+            edad = Integer.parseInt(txtEdad);
+        } catch (NumberFormatException x) {
+            throw new ErrorEnEdadException(txtEdad);
+        }
         Persona persona;
         if (isAmigo){
             persona = new Amigo(nombre, cedula, edad, riesgo, relacion, facebook);
